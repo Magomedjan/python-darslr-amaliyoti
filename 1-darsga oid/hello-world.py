297169bac275ff9ab9085fa438beddc2357d90d8
Magomedjan <salasax78@gmail.com>
2024-02-24 23:10:04 +0500

    yangi dars kodlari
    1 va 3 darslar

---
+++ 1-darsga oid/hello-world.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 07:26:22 2023
+
+@author: User
+"""
+
+print("Hello world!")
+print("Hello my family and my friends")
+print(2*2)
+print(99*22-34+88994)
+print('Assalomu alaykum')
+print('XAYRLI TONG')
+print(19/5)
+print(2**4)
