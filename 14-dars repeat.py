b2179914d6ca4e8ded6661b7fa2215f25b426ff8
Magomedjan <salasax78@gmail.com>
2024-02-25 11:42:38 +0500

    yangi kod
    14-dars mashq kodlari

---
+++ 14-dars repeat.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 08:40:45 2024
+
+@author: Magomedjan
+"""
+                # bo'sh lug'at yaratish, qiymat qo'shish, o'zgartitrish va qiymatni o'chirish
+talaba_1={}
+
+talaba_1['ism'] = "xujamuruodov boburjon"
+talaba_1['t_yil'] = 2003
+talaba_1['t_kun'] = 10.09
+talaba_1['t_joy'] = "buxoro"
+talaba_1['tuman'] = 'shofirkon'
+talaba_1['nation'] = "o'zbek"
+talaba_1['inst'] = "Diplomatiya"
+talaba_1['u_joy'] = 'Toshkent'
+talaba_1['fakultet'] = 'jahon iqtisodiyoti'
+talaba_1['kurs'] = 4
+print(f"{talaba_1['ism'].title()} {talaba_1['t_kun']}.{talaba_1['t_yil']} yil \
+{talaba_1['t_joy'].title()} viloyati {talaba_1['tuman'].title()} tumanida tug'ilgan, \
+millati {talaba_1['nation']}, {talaba_1['inst']} universitetining {talaba_1['fakultet'].title()} \
+fakultetini {talaba_1['kurs']} bosqich talabasi")
