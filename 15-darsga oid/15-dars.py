e012eb03b2d34908a8bad53f2b2a70867cef0748
Magomedjan <salasax78@gmail.com>
2024-03-04 07:03:40 +0500

    yangi qushimchalar
    15-dars va spravochnik qushildi

---
+++ 15-darsga oid/15-dars.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 20:16:02 2024
+
+@author: Magomedjan
+"""
+#                                    15-dars
+
+talaba_1={}
+
+talaba_1['ism'] = "xujamuruodov boburjon"
+talaba_1['t_yil'] = 2003
+talaba_1['t_kun'] = 10.09
+talaba_1['t_joy'] = "buxoro"
+talaba_1['tuman'] = 'shofirkon'
+talaba_1['nation'] = "o'zbek"
+talaba_1['inst'] = "Diplomatiya"
+talaba_1['u_joy'] = 'Toshkent'
+talaba_1['fakultet'] = 'jahon iqtisodiyoti'
+talaba_1['kurs'] = 4
+print(talaba_1.items())
+for k, q in talaba_1.items():
+    print(f"   kalit: {k}")
+    print(f"   qiymat:{q} \n")
+    
---
+++ python spravochnik.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 20:18:01 2024
+
+@author: Magomedjan
+"""
+                        #PYTHON BO"YICHA SPRAVOCHNIK 
+Python = {'integer':'butun son', 
+            'float':'unlik son', 
+            'string':'matn', 
+            'if':"agar funk", 
+            'else':'aks holda',
+            'for':"uchun",
+            'in':"ichida",
+            'del':"yo'q qilish",
+            'title':"bosh harf",
+            'input':"foydalnuvchi qiymat kiritishi",
+            'print':"ekranga chqarish",
+                   'items()':"lugat elementlarini chiqarish",
+                   
+                   
+                   }
+
+  
+         lugat = input("pythonga oid suz kiriting: \n>>>").lower()
+         print(Python.get(lugat,"Bunda so'z mavjud emas" ))
+         
+         #Yuqoridagi vazifani if-else yordamida qiling va natijani ham foydalanuvchiga tushunarli 
+         #ko'rinishda chiqaring.
+
+         lg = input("pytonda tushunmagan komandani kiriting \n>>>").lower()
+         kalit = Python.get(lg)
+         if kalit == None:
+             print("Bunda so'z mavjud emas")
+         else:
+             print(f"'{lg.title()}' suzini ma'nosi '{kalit}' so'zini bildiradi")
