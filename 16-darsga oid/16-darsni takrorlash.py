29848db8c130fff1fa21ee49344e1a3314be5126
Magomedjan <salasax78@gmail.com>
2024-03-21 22:42:08 +0500

    15 mashq
    15 mashq, qo'shimcha takrorlash

--- 16-darsga oid/16-darsni takrorlash.py
+++ 16-darsga oid/16-darsni takrorlash.py
@@ -3,7 +3,14 @@
 Created on Tue Mar 12 07:43:31 2024
 
 @author: Magomedjan
+
 """
+
+
+
+                            #NESTING
+
+
 car0 = {
         'model':'lacetti',
         'rang':'oq',
@@ -31,11 +38,16 @@ car2 = {
         'korobka':'mexanika'
         }
 
+
+                        # Bitta mashinani ma'lumotlarini chiqarish
 #car=car2
 #print(f"{car['model'].title()}, "
 #      f"{car['rang']} rang, "
 #      f"{car['yil']} yil, {car['narx']}$"
 #      )
+
+
+                        #Ro'yxat ichida lug'at yaratish                        
 
 cars = [car0, car1, car2]
 for car in cars:
@@ -48,47 +60,7 @@ print(f"{cars[2]['rang'].title()} "
          )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    #Ro'yxat orqali lug'at yaratishni optom ishlashga qulayligi
 
 malibus = []
 for n in range(50):
@@ -101,7 +73,123 @@ for n in range(50):
         "korobka":"avto"
         }
     malibus.append(new_car)
+
+
+for malibu in malibus[:15]:
+    malibu['rangi']=['qizil']
+
 #for malibu in malibus:
 #    print(malibu)
+
+for malibu in malibus[15:30]:
+    malibu['rangi']=['qora']
+    
+for malibu in malibus[30:]:
+    malibu['rangi']=['qora']
+    malibu['korobka']=['mexanika']
+   
+#for malibu in malibus:
+#    print(malibu)
+
+for malibu in malibus:
+    if malibu['korobka']=='avto':
+        malibu['narxi']=40000
+    else:
+        malibu['narxi']=35000
+
+for malibu in malibus:
+    print(malibu)
+
+
+                    #Lug'at ichida ro'yxat yaratish
+                    
+
+dasturchilar = {
+                'ali':['c++', 'python'],
+                'vali':['js', 'css', 'html'],
+                'nabi':['php', 'SQL'],
+                "g'ani":['python', 'php'],
+                'maryam':['c++', 'c#']
+                }
+
+for ism, tillar in dasturchilar.items():
+    print(f"\n{ism.title()} quyidagi dasturlash tillarini biladi : ", end='' )
+    for til in tillar:
+        print(f" {til.upper()} " , end='' )
+        
+        
+                    #LUG'AT ICHIDA LUG'AT YARATISH
+hamkasblar = {
+                'ali':{'familiya':'valiev',
+                       'tyil':1995,
+                       'malumot':'oliy',
+                       'tillar':['c++', 'python']},
+                'vali':{'familiya':'aliev',
+                       'tyil':2000,
+                       'malumot':"o'rta maxsus",
+                       'tillar':['js', 'css', 'html']},
+                'hasan':{'familiya':'husanov',
+                       'tyil':2003,
+                       'malumot':"o'rta",
+                       'tillar':['php', 'SQL']}
+            }
+
+for ism, info in hamkasblar.items():
+    print(f"\n{ism.title()} {info['familiya'].title()}."
+          f"{info['tyil']} yilda tug'ilgan."
+          f"\nMa'lumoti: {info['malumot']}. \nQuyidagi tillarni biladi: ")
+    for til in info['tillar']:
+        print(til.upper())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
