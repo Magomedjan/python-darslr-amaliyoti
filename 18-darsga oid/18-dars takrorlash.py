13a7902180c4db1ffa57d2ecbd76b96624f88c70
Magomedjan <salasax78@gmail.com>
2024-05-16 22:43:24 +0500

    18 va 19-darslarga oid
    18 dars amaliy va 19- dars takrorlash

--- 18-darsga oid/18-dars takrorlash.py
+++ 18-darsga oid/18-dars takrorlash.py
@@ -65,7 +65,64 @@ Created on Tue Apr 30 07:27:14 2024
 
 # 1/Foydalanuvchidan buyurtma qabul qiluvchi dastur yozing. 
 #   Mahsulotlar nomini birma-bir qabul qilib, yangi ro'yxatga joylang.
-print("Buyurtma olish dasturi")
+#print("Buyurtma olish dasturi")
+#zakaz = []
+##n = 1
+#while True:
+#    mahsulot = input("Savatga kerakli mahsulotni kiriting: ")
+#    print(f"{n}-mahsulot tanlandi")
+##    zakaz.append(mahsulot)
+#    n+=1
+#    takror_s = input("Yana mahsulot tanlaysizmi? \nha/yo'q: ")
+#    if takror_s != 'ha':
+#        break
+#print("Siz tanlagan mahsulotlar: ")
+#for z in zakaz:
+#    print(f" - {z} ")    
+
+# 2/e-bozor uchun mahsulotlar va ularning narhlari lug'atini 
+#   shakllantiruvchi dastur yozing. Foydalanuvchidan lug'atga bir 
+#   nechta elementlar (mahsulot va uning narhi) kiritishni so'rang.
+#print("e-bozor dasturi")
+#e_bozor = {}
+#n=1
+#ishora = True
+#while ishora:
+#    mahsulot = input(f"{n}-mahsulotni kiriting: ")
+#    narx = int(input(f"{mahsulot} narxini yozing: "))
+#    e_bozor[mahsulot]=narx
+#    tsurov = input("Yana mahsulot qo'shasizmi? ha/yo'q: ")
+#    n += 1
+#    if tsurov == "yo'q":
+#        ishora = False
+#print("Sizni mahsulatlaringiz ro'yxati:")
+#for m,n in e_bozor.items():
+#    print(f" - {m} mahsuloti, narxi {n} so'm.")
+
+
+
+# 3/Yuqoridagi ikki dasturni jamlaymiz. Foydalanuvchi buyurtmasi 
+#   ro'yxatidagi har bir mahsulotni e-bozordagi mahsulotlar 
+#   bilan solishitiring (tayyor ro'yxat ishlatishingiz mumkin). 
+#   Agar mahsuot e-bozorda mavjud bo'lsa mahuslot narhini chiqaring, 
+#   aks holda "Bizda bu mahsulot yo'q" degan xabarni kor'sating.
+    
+
+e_bozor = {}
+n=1
+ishora = True
+while ishora:
+    mahsulot = input(f"{n}-mahsulotni kiriting: ")
+    narx = int(input(f"{mahsulot} narxini yozing: "))
+    e_bozor[mahsulot]=narx
+    tsurov = input("Yana mahsulot qo'shasizmi? ha/yo'q: ")
+    n += 1
+    if tsurov == "yo'q":
+        ishora = False
+print("e-bozordagi mahsulatlar ro'yxati:")
+for m,n in e_bozor.items():
+    print(f" - {m} mahsuloti, narxi {n} so'm.")
+
 zakaz = []
 n = 1
 while True:
@@ -76,42 +133,15 @@ while True:
     takror_s = input("Yana mahsulot tanlaysizmi? \nha/yo'q: ")
     if takror_s != 'ha':
         break
-print("Siz tanlagan mahsulotlar: ")
-for z in zakaz:
-    print(f" - {z} ")    
+while zakaz:
+    buyurtma = zakaz.pop()
+    if buyurtma in e_bozor.keys():
+        narx = e_bozor[buyurtma]
+        print(f" - {buyurtma.title()} - {narx} so'm")
+    else:
+        print(f"Kechirasiz, {buyurtma} mahsuloti bizda mavjid emas")
+  
 
-# 2/e-bozor uchun mahsulotlar va ularning narhlari lug'atini 
-#   shakllantiruvchi dastur yozing. Foydalanuvchidan lug'atga bir 
-#   nechta elementlar (mahsulot va uning narhi) kiritishni so'rang.
-
-
-
-
-# 3/Yuqoridagi ikki dasturni jamlaymiz. Foydalanuvchi buyurtmasi 
-#   ro'yxatidagi har bir mahsulotni e-bozordagi mahsulotlar 
-#   bilan solishitiring (tayyor ro'yxat ishlatishingiz mumkin). 
-#   Agar mahsuot e-bozorda mavjud bo'lsa mahuslot narhini chiqaring, 
-#   aks holda "Bizda bu mahsulot yo'q" degan xabarni kor'sating.
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
---
+++ 19-darsga oid/19-dars takrorlash.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 22:33:06 2024
+
+@author: Magomedjan
+
+"""
+
+def salom_ber():
+    """Salom berish funktsiyasi"""
+    print("Assalomu alaykum")
+
+salom_ber()
+
+
