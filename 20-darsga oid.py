a16587fa96158f3935096bf89e06de91548da500
Magomedjan <salasax78@gmail.com>
2024-05-30 00:01:28 +0500

    20-dars qushimcha
    20 darga qushimcha, takrorlash

--- 20-darsga oid.py
+++ 20-darsga oid.py
@@ -17,18 +17,125 @@ Created on Sun May 26 22:46:46 2024
 #print(f"Talaba {talaba3} darsga kechikib keldi")
 
 
-def tuliq_ism_chiqar(ism, familiya, sharif=''):
-    """Tuliq ism qaytarish funksiyasi"""
-    if sharif:
-        tuliq_ism = f"{ism} {sharif} {familiya}"
+#def tuliq_ism_chiqar(ism, familiya, sharif=''):
+#    """Tuliq ism qaytarish funksiyasi"""
+#    if sharif:
+#        tuliq_ism = f"{ism} {sharif} {familiya}"
+#    else:
+#        tuliq_ism = f"{ism} {familiya}"
+#    return tuliq_ism.title()
+#talaba1 = tuliq_ism_chiqar('fayzullo', 'Ismatov', 'salimovich')
+#talaba2 = tuliq_ism_chiqar("Olimjon", "ganiev")
+#talaba3 = tuliq_ism_chiqar('Madi', 'Soliev', 'Alievich')
+#print(f"Darsga kelmagan talabalar: \n -{talaba1} \n -{talaba2}")    
+#print(f"Talaba {talaba3} darsga kechikib keldi")
+
+
+#def avto_info(Kompaniya, modeli, rangi, uzqutisi, yili, narxi=None):
+#    """avto bozor"""
+#    avto = {'Kompaniya':Kompaniya,
+#            'Model':modeli,
+#            'Rang':rangi,
+#            'Korobka':uzqutisi,
+#            'Yil':yili,
+#            'Narx':narxi            
+#            }
+##    return avto
+#avto1 = avto_info('GM', 'Malibu', 'oq', 'avtomat', 2015)
+##avto2 = avto_info('GM', 'Lacetti', 'qora', 'mexanika', 2023, 15000)
+#avtolar = [avto1, avto2]
+#print(avtolar[0]['Model'])
+##print(avto1['Narx'])
+#print(avto2['Narx'])
+#print("Online bozorda mavjud avomashina modellari:")
+#for avto in avtolar:
+#    if avto['Narx']:
+#        narx = avto['Narx']
+#    else:
+#        narx = "Noma'lum"
+#    print(f"{avto['Rang'].title()} {avto['Model']} narxi: {narx} ")
+
+
+
+#def oraliq(min, max, qadam = ''):
+#    sonlar=[]
+#    while min<max:
+#        if qadam:
+#            min += qadam
+#        else:
+#            min += 1
+#        sonlar.append(min)
+#    return sonlar        
+        
+#print(oraliq(0, 12, 4))
+#print(oraliq(0, 12, 3))
+#print(oraliq(0, 12, 2))
+#print(oraliq(0, 12))
+
+
+
+
+def avto_info(Kompaniya, modeli, rangi, uzqutisi, yili, narxi=None):
+    """avto bozor"""
+    avto = {'Kompaniya':Kompaniya,
+            'Model':modeli,
+            'Rang':rangi,
+            'Korobka':uzqutisi,
+            'Yil':yili,
+            'Narx':narxi            
+            }
+    return avto
+print("Avtosalonda mavjud avtomashinalar ro'yxatini shakllantiramiz")
+avtolar = []
+while True:
+    print("Quyidagi ma'lumotlarni kiriting!")
+    Kompaniya = input('Avtokompaniya nomi: ')
+    modeli = input('modeli: ')
+    rangi = input('rangi: ')
+    uzqutisi = input('uzatmalar qutisi turi: ')
+    yili = input("Ishlab chiqarilgan yili: ")
+    narxi= input('narxi: ')
+    avtolar.append(avto_info(Kompaniya, modeli, rangi, uzqutisi, yili))
+    surov = input('Yana avto kiritasizmi? y/n ')
+    if surov == 'n':
+        break
+print("Salonda mavjud avomashina modellari:")
+for avto in avtolar:
+    if avto['Narx']:
+        narx = avto['Narx']
     else:
-        tuliq_ism = f"{ism} {familiya}"
-    return tuliq_ism.title()
-talaba1 = tuliq_ism_chiqar('fayzullo', 'Ismatov', 'salimovich')
-talaba2 = tuliq_ism_chiqar("Olimjon", "ganiev")
-talaba3 = tuliq_ism_chiqar('Madi', 'Soliev', 'Alievich')
-print(f"Darsga kelmagan talabalar: \n -{talaba1} \n -{talaba2}")    
-print(f"Talaba {talaba3} darsga kechikib keldi")
+        narx = "Noma'lum"
+    print(f"{avto['Kompaniya'].title()} kompaniyasini {avto['Model'].title()} "
+          f" avtomasinasi< rangi - {avto['Rang']}, {avto['Yil']} yilda i/ch, "
+          f"uzatmalar qutisi - {avto['Korobka']}, narxi - {narx}$")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
