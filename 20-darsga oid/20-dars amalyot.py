b50f58198bbdc3b6b0651424c41397e3075982d7
Magomedjan <salasax78@gmail.com>
2024-06-02 23:25:31 +0500

    20 darsga qushimcha
    20-dars amaliyoti

--- 20-darsga oid/20-dars amalyot.py
+++ 20-darsga oid/20-dars amalyot.py
@@ -12,13 +12,76 @@ Created on Fri May 31 22:42:15 2024
 #    qaytaruvchi funksiya yozing. Lug'atda foydalanuvchu yoshi ham bo'lsin. 
 #    Ba'zi argumentlarni kiritishni ixtiyoriy qiling (masalan, tel.raqam, el.manzil)
 
+#def user_info(ismi, familiyasi, t_yili, t_joyi, telefoni, e_maili=''):
+#    """Mijoz haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+#    user = {'ism':ismi,
+#            'familiya':familiyasi,
+#            't_yil':t_yili,
+#            't_joy':t_joyi,
+#            'telefon':telefoni,
+#            'e_mail':e_maili           
+#            }
+#    return user
+#user1 = user_info('fayzullo', 'ismatov', 1972, 'vardonze', 997011111, 'dollar@gmail.com')
+##print(f"{user1['ism'].title()} {user1['familiya'].title()}"
+#      f" {user1['t_joy'].title()}da tug'ilgan, yoshi {2024-user1['t_yil']}da ")
+    
+   
 
 # 2/ Yuqoridagi funksiyani while yordamida bir necha bor chaqiring, 
 #    va mijozlar degan ro'yxatni shakllantiring. Ro'yxatdagi mijozlar 
 #    haqidagi ma'lumotni konsolga chiqaring.
 
+#def user_info(ismi, familiyasi, t_yili, t_joyi, telefoni=None, e_maili=''):
+#    """Mijoz haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+#    user = {'ism':ismi,
+#            'familiya':familiyasi,
+#            't_yil':t_yili,
+#            'yosh':2024 - t_yili,
+#            't_joy':t_joyi,
+#            'telefon':telefoni,
+#            'e_mail':e_maili           
+#            }
+#    return user
+##mijozlart bush ruyxatini shakillantirish
+#mijozlar=[]
+#while True:
+#    print("Mijoz ma'lumotlarini kirirting: ")
+#    ismi=input('Ismi: ')
+#    familiyasi=input("Familiyasi: ")
+#    t_yili=int(input("Tug'ilgan yili: "))
+#    t_joyi=input("Tugilgan joyi: ")
+#    telefoni=input("Telefoni ")
+#    e_maili=input("e-mail: ")
+#    mijozlar.append(user_info(ismi, familiyasi, t_yili, t_joyi, telefoni, e_maili))
+#    surov = input("Yana mijoz qo'shsizmi? h/y: ")
+#    if surov =='y':
+#        break
+#print("Mavjud mijoz haqida ma'lumotlar: ")
+#for mijoz in mijozlar:
+#    if mijoz['e_mail']:
+#        e_mail=mijoz['e_mail']
+#    else:
+#        e_mail="mavjud emas"
+#    print(f" - {mijoz['ism'].title()} {mijoz['familiya'].title()} {mijoz['t_yil']} yil"
+#          f" {mijoz['t_joy']}da tug'ilgan, {mijoz['yosh']} yoshda, telefoni:"
+#          f" {mijoz['telefon']}, e-maili:{e_mail}") 
+           
+
+
 
 # 3/ Uchta son qabul qilib, ulardan eng kattasini qaytaruvchi funksiya yozing
+
+
+def kattasini_ber(x, y, z):
+    """son qabul qilib, ulardan eng kattasini qaytaruvchi funksiya"""
+    
+
+
+
+
+
+
 
 
 # 4/ Foydalanuvchidan aylaning radiusini qabul qilib olib, uning radiusini, 
